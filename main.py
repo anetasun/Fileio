ca1d375b72b3a65de8817da4444313b44d19978f
anetasun <anetsn@gmail.com>
2024-12-04 13:19:20 +0300

    Заготовка проекта с функцией upload для загрузки изобрадения на сайт

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import ttk
+import requests
+from bottle import response
+
+
+def upload():
+    filepath = fd.askopenfilename()
+    if filepath:
+        files = {'file': open(filepath, 'rb')}
+        response = requests.post('http://file.io', files=files)
+        if response.ststus_code == 200:
+            link = response.json()['link']
+            entry.insert(0, link)
+
+
+
+window = Tk()
+window.title("Сохранение файла в облаке")
+window.geometry("400x200")
+
+button = ttk.Button(text="Загрузить файл", command=upload)
+button.pack()
+
+
+entry = ttk.Entry()
+entry.pack()
+
+window.mainloop()
